468dd755995722835cf227bfe4a423f55233fe14
pastelito08 <pastelito2062@gmail.com>
2026-05-30 18:07:27 +0545

    fix CORS allow all

--- app/main.py
+++ app/main.py
@@ -15,12 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://social-frontend-puce.vercel.app",
-        "*"
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
